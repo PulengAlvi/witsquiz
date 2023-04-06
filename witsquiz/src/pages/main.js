0dd0a5bc0063a973df51dacb6db6ff691b25d48c
PulengAlvi <2213773@students.wit.ac.za>
2023-04-06 12:29:03 +0200

    modified:   witsquiz/package-lock.json 	modified:   witsquiz/package.json 	new file:   witsquiz/public/favicon.ico 	modified:   witsquiz/public/index.html 	modified:   witsquiz/src/App.css 	modified:   witsquiz/src/components/navbar.js 	new file:   witsquiz/src/pages/account.js 	modified:   witsquiz/src/pages/main.js 	new file:   witsquiz/travis.yml

--- witsquiz/src/pages/main.js
+++ witsquiz/src/pages/main.js
@@ -1,5 +1,7 @@
 export const Main = () => {
+    //Main method returns layout of html home page
     return <div>
          <h1> WitsQuiz Home Page</h1>
+         <img src={"/logo2.jpg"} alt = "/logo1.jpg" width="600" height="600" />
          </div>;
   };
