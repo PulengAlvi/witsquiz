55a796b97c0e0ce45e24b52018622a9adfa4f417
Okuhle Qina <okuhleqina15@gmail.com>
2023-04-23 17:15:45 +0200

    added create quiz page,add question page and can view quizzes in the home page

--- witsquiz/src/components/category.js
+++
@@ -1,9 +0,0 @@
-const Categories=[
-    {category :"General Knowledge", value:0},
-    {category :"Books", value:1},
-    {category :"Films", value:2},
-    {category :"Music", value:3},
-    {category :"Sports", value:4},
-
-];
-export default Categories;
